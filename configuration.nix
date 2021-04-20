298d197a0957aec5525c9c52b47f8ab813265af3
qvshuo <qvshuo@foxmail.com>
2021-04-20 18:30:23 +0800

    sort by arch install guide

--- configuration.nix
+++ configuration.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
@@ -14,132 +10,130 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  # Define your hostname.
-  networking.hostName = "nixos";
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Set your time zone.
-  time.timeZone = "Asia/Shanghai";
-
-  # Use hardware clock in local time instead of UTC
-  # This is required for compatibility with windows
+  # Use hardware clock in local time instead of UTC.
   time.hardwareClockInLocalTime = true;
 
+  # Set time zone.
+  time.timeZone = "Asia/Shanghai";
 
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
-  networking.useDHCP = false;
-  networking.interfaces.wlp2s0.useDHCP = true;
+  # Set location.
+  location = {
+    latitude = 34.0;
+    longitude = 108.0;
+  };
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  # };
-
+  # Localization.
   i18n = {
     defaultLocale = "zh_CN.UTF-8";
     supportedLocales = [ "zh_CN.UTF-8/UTF-8" "ja_JP.UTF-8/UTF-8" "en_US.UTF-8/UTF-8" ];
     inputMethod = {
       enabled = "ibus";
-      ibus.engines = with pkgs.ibus-engines; [ rime mozc ];
+      ibus.engines = with pkgs.ibus-engines; [ libpinyin mozc ];
     };
   };
 
-  # Completely disable the NVIDIA graphics card and use the integrated graphics processor instead. 
-  # https://nixos.org/manual/nixos/stable/options.html#opt-hardware.nvidiaOptimus.disable
-  hardware.nvidiaOptimus.disable = true;
+  # Define hostname.
+  networking.hostName = "nixos";
+
+  # Define a user account.
+  users.users.huizhi = {
+    isNormalUser = true;
+    home = "/home/huizhi";
+    extraGroups = [ "wheel" "networkmanager" ];
+    description = "Huizhi Yang";
+    uid = 1000;
+  };
+
+  # Whether users of the wheel group must provide a password to run commands as super user via sudo. 
+  security.sudo.wheelNeedsPassword = false; 
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
+  # Configure keymap in X11
+  services.xserver.layout = "us";
 
-  # Enable the GNOME 3 Desktop Environment.
-  # services.xserver.displayManager.gdm.enable = true;
-  # services.xserver.desktopManager.gnome3.enable = true;
-  
+  # HiDPI.
+  services.xserver.dpi = 120;
+  # Enable Font/DPI configuration optimized for HiDPI displays.
+  hardware.video.hidpi.enable = true;
+
+  # Fonts.
+  fonts.enableDefaultFonts = true;
+  fonts.fonts = with pkgs; [
+    sarasa-gothic
+  ];
+
   # Enable the Pantheon Desktop Environment.
   services.xserver.desktopManager.pantheon.enable = true;
 
+  # Enable xdg desktop integration.
+  xdg.portal.enable = true;
+  # List of additional portals to add to path.
+  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
 
-  # Configure keymap in X11
-  services.xserver.layout = "us";
-  # services.xserver.xkbOptions = "eurosign:e";
+  # A set of environment variables used in the global environment.
+  environment.variables = { EDITOR = "nvim"; VISUAL = "nvim"; };
 
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
+  # Update the CPU microcode for Intel processors.
+  hardware.cpu.intel.updateMicrocode = true;
 
   # Enable sound.
   sound.enable = true;
-  hardware.pulseaudio.enable = true;
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  services.xserver.libinput.enable = true;
+  # Video drivers
+  services.xserver.videoDrivers = [ "modesetting" ];
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.huizhi = {
-    isNormalUser = true;
-    home = "/home/huizhi";
-    # Enable ‘sudo’ for the user.
-    extraGroups = [ "wheel" "networkmanager" ];
-    uid = 1000;
-  };
+  # Completely disable the NVIDIA graphics card and use the integrated graphics processor instead. 
+  hardware.nvidiaOptimus.disable = true;
 
+  #  Enable the TLP power management daemon.
+  services.tlp.enable = true;
 
-  nix = {
-    binaryCaches = [ 
-      "https://mirrors.bfsu.edu.cn/nix-channels/store"
-    ];
-  };
+  # Whether to enable periodic SSD TRIM of mounted partitions in background.
+  services.fstrim.enable = true;
 
+  # Garbage collector.
+  nix.gc.automatic = true;
+  nix.gc.options = "--delete-older-than 15d";
+
+  # List of binary cache URLs used to obtain pre-built binaries of Nix packages.
+  nix.binaryCaches = [ "https://mirrors.bfsu.edu.cn/nix-channels/store" ];
+
+  # Allow unfree.
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
+  # The set of packages that appear in /run/current-system/sw.
   environment.systemPackages = with pkgs; [
+    clash
     dosfstools
+    drawio
     firefox
     fish
+    hugo
+    gimp
     git
     manpages
     neofetch
     neovim
+    nodejs
     ntfs3g
+    proxychains
+    scrcpy
+    tdesktop
     texinfo
+    typora
+    wpsoffice
+    you-get
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
+  # Whether to enable the Vixie cron daemon.
+  services.cron.enable
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "20.09"; # Did you read the comment?
-
+  # The NixOS release.
+  system.stateVersion = "20.09";
 }
 
