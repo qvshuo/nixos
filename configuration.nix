1db6c44a595beae611678734338da5487711d819
qvshuo <qvshuo@foxmail.com>
2021-06-28 00:07:25 +0800

    one file

--- configuration.nix
+++ configuration.nix
@@ -4,17 +4,76 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      ./imports/boot.nix
-      ./imports/de.nix
-      ./imports/hardware.nix
-      ./imports/localization.nix
-      ./imports/user.nix
     ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+
+  # Update the CPU microcode for Intel processors.
+  hardware.cpu.intel.updateMicrocode = true;
+
+  # GPU acceleration.
+  hardware.opengl.extraPackages = with pkgs; [
+    intel-compute-runtime
+  ];
+
+  # Enable Font/DPI configuration optimized for HiDPI displays.
+  hardware.video.hidpi.enable = true;
+
+  # Enable pipewire.
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+
+  # Use hardware clock in local time instead of UTC.
+  time.hardwareClockInLocalTime = true;
+
+  # Set time zone.
+  time.timeZone = "Asia/Shanghai";
+
+  # Language and input method.
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    inputMethod = {
+      enabled = "ibus";
+      ibus.engines = with pkgs.ibus-engines; [
+        rime
+      ];
+    };
+  };
+
+  # Enable the X server.
+  services.xserver.enable = true;
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  services.gnome.core-utilities.enable = false;
+
+
+  # Add user.
+  users.users.huizhi = {
+    isNormalUser = true;
+    home = "/home/huizhi";
+    extraGroups = [ "wheel" "networkmanager" ];
+    description = "杨蕙芷";
+    uid = 1000;
+  };
+  security.sudo.wheelNeedsPassword = false;
+
 
   # Garbage collector.
   nix.gc.automatic = true;
-  nix.gc.dates = "03:15";
+  nix.gc.dates = "04:00";
   nix.gc.options = "--delete-older-than 7d";
+
 
   # List of binary cache URLs used to obtain pre-built binaries of Nix packages.
   nix.binaryCaches = [ "https://mirror.sjtu.edu.cn/nix-channels/store" ];
@@ -24,7 +83,17 @@
 
   # The set of packages that appear in /run/current-system/sw.
   environment.systemPackages = with pkgs; [
-    alacritty
+    gnome.eog
+    gnome.file-roller p7zip unrar
+    gnome.nautilus gnome.sushi ffmpegthumbnailer 
+    gnome.gnome-terminal
+    gnome.gnome-tweaks
+    gnomeExtensions.blur-my-shell
+    gnomeExtensions.clipboard-indicator
+    gnomeExtensions.just-perfection
+    gnomeExtensions.night-theme-switcher
+    gnomeExtensions.shu-zhi
+    gnomeExtensions.window-is-ready-notification-remover
     clash
     fish
     hugo
@@ -45,5 +114,6 @@
 
   # The NixOS release.
   system.stateVersion = "21.05";
+
 }
 
